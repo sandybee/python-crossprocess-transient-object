caa58313c1cfabfdca51b7e9285f6b5237c970fe
Eric Villard <dev@eviweb.fr>
2016-06-02 15:00:46 +0200

    add process module + crossprocess tests
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ crossprocess/process.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import sys
+import os
+
+from .serialization import Serializer
+
+
+def run_script(script, object=None):
+    """
+    run a given script
+    """
+    os.environ['PYTHONPATH'] = os.pathsep.join(sys.path)
+    if object:
+        os.environ[object.__class__.__name__] = Serializer.serialize(
+            object).decode()
+
+    proc = subprocess.Popen(
+        [sys.executable, script],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=os.environ
+    )
+
+    result = proc.communicate()
+
+    exit_status = proc.wait()
+    if exit_status != 0:
+        raise Exception("script failed (exit status: %d)" % exit_status)
+
+    return result
---
+++ tests/test_crossprocess.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+import sys
+import shutil
+import tempfile
+
+from crossprocess.process import run_script
+from crossprocess.objects import SimpleObject
+
+
+class TestCrossprocess(object):
+
+    def get_script(self, name, extension='.py'):
+        """
+        helper method to get a fixture script
+        :param name: script name without extension
+        :param extension: script extension
+        """
+        return os.path.join(
+            os.path.dirname(__file__),
+            'fixtures',
+            name + extension
+        )
+
+    def read_from_stdout(self, result):
+        """
+        helper method to extract the stdout string
+        :param result: the result tuple returned from run_script
+        """
+        return result[0].decode().strip()
+
+    def copy_script(self, script):
+        """
+        helper method to create a temporary copy of a given script
+        :param script: script to copy
+        """
+        tmpfile = tempfile.NamedTemporaryFile()
+        shutil.copy(script, tmpfile.name)
+
+        return tmpfile
+
+    def test_run_script(self):
+        """
+        run a simple script
+        """
+        assert self.get_script('simple') == self.read_from_stdout(
+            run_script(self.get_script('simple')))
+
+    def test_run_script_handle_fails(self):
+        """
+        ensure failing script is handled
+        """
+        with pytest.raises(Exception):
+            run_script(self.get_script('failing'))
+
+    def test_run_script_keeps_sys_path(self):
+        """
+        cross process sys path
+        """
+        script = self.get_script('syspath')
+        expected = [os.path.dirname(script)] + sys.path
+        assert "\n".join(expected).strip() == self.read_from_stdout(
+            run_script(script))
+
+    def test_run_script_permits_transient_module(self):
+        """
+        project module is accessible from a subprocess
+        """
+        expected = 'transient_module'
+        script = self.get_script(expected)
+
+        assert expected == self.read_from_stdout(run_script(script))
+
+    def test_run_copy_of_script(self):
+        """
+        project module is accessible from a subprocess even for copy
+        """
+        fscript = self.copy_script(self.get_script('transient_module'))
+        expected = os.path.basename(fscript.name)
+        assert expected == self.read_from_stdout(run_script(fscript.name))
+
+    def test_run_script_persistence(self):
+        """
+        object internal persistence
+        """
+        script = self.get_script('persistent')
+        obj = SimpleObject('persistent')
+        assert obj.get_name() == self.read_from_stdout(
+            run_script(script, obj))
